d2c8ed7d45d09152a9fa93793ab8b1d665d6bbb8
IbrahimHarry <harris.ibrahim@outlook.com>
2022-09-25 23:19:24 +0530

    search bar and live weather status completed

--- src/api.js
+++ src/api.js
@@ -1,9 +1,27 @@
-export const geoApiOptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'ce673c26a0msh1ac6465e6ab6fbcp1066acjsn77c7eb8b2b04',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
-};
-export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+// export const geoApiOptions = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'ce673c26a0msh1ac6465e6ab6fbcp1066acjsn77c7eb8b2b04',
+// 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+// 	}
+// };
+// export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
 
+// export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+// export const WEATHER_API_KEY = "d2e07f52f6c8affce860b27979ad2fb6";
+
+
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "ce673c26a0msh1ac6465e6ab6fbcp1066acjsn77c7eb8b2b04",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "d2e07f52f6c8affce860b27979ad2fb6";
+
+
+  
